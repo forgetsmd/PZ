2adfbb65870787bc246ea2846fc0f4f816ca03d3
Forgetsmd <forgetsmd@gmail.com>
2021-10-28 12:32:15 +0300

    MoveTryCatch

--- 2PZ.cpp
+++ 2PZ.cpp
@@ -13,59 +13,77 @@ public:
 
 	DArr(int size)
 	{
-		while (true)
+		this->size = size;
+		p_DArr = new T[size];
+		for (int i = 0; i < size; i++)
 		{
-			try
-			{
-				this->size = size;
-				p_DArr = new T[size];
-				for (int i = 0; i < size; i++)
-				{
-					p_DArr[i] = i;
-				}
-				break;
-			}
-			catch (const std::bad_alloc&)
-			{
-				cout << "Error: bad_alloc" << endl;
-			}
+			p_DArr[i] = i;
 		}
+
 		initialized = true;
 	}
 
 	DArr(const DArr& copmas)
 	{
-		while (true)
+		if (copmas.initialized)
 		{
-			try
+			this->size = copmas.size;
+			p_DArr = new T[size];
+			for (int i = 0; i < size; i++)
 			{
-				if (copmas.initialized)
-				{
-					this->size = copmas.size;
-					p_DArr = new T[size];
-					for (int i = 0; i < size; i++)
-					{
-						this->p_DArr[i] = copmas.p_DArr[i];
-					}
-					this->initialized = true;
-				}
-				else
-				{
-					cout << "Error: Copied array does not initialize" << endl;
-				}
-				break;
+				this->p_DArr[i] = copmas.p_DArr[i];
 			}
-			catch (const std::bad_alloc&)
+			this->initialized = true;
+		}
+		else
+		{
+			cout << "Error: Copied array does not initialize" << endl;
+		}
+	}
+
+	friend DArr operator  + (const DArr& a, const DArr& b)
+	{
+		DArr temp;
+
+		if (a.size < b.size)
+		{
+			temp.init(a.size);
+			for (int i = 0; i < a.size; i++)
 			{
-				cout << "Error: bad_alloc" << endl;
+				temp.p_DArr[i] = a.p_DArr[i] + b.p_DArr[i];
 			}
 		}
 
+		else
+		{
+			temp.init(b.size);
+			for (int i = 0; i < a.size; i++)
+			{
+				temp.p_DArr[i] = a.p_DArr[i] + b.p_DArr[i];
+			}
+		}
+
+		return temp;
 	}
 
-	friend DArr operator  + (const DArr& a, const DArr& b);
-	void operator = (const DArr& a);
-	
+	void operator = (const DArr& a)
+	{
+		if (a.initialized)
+		{
+			this->size = a.size;
+			p_DArr = new T[size];
+			for (int i = 0; i < size; i++)
+			{
+				this->p_DArr[i] = a.p_DArr[i];
+			}
+			this->initialized = true;
+		}
+		else
+		{
+			cout << "Error: Copied array does not initialize" << endl;
+		}
+	}
+
 
 	~DArr()
 	{
@@ -75,22 +93,11 @@ public:
 
 	void init(int size)
 	{
-		while (true)
+		this->size = size;
+		p_DArr = new T[size];
+		for (int i = 0; i < size; i++)
 		{
-			try
-			{
-				this->size = size;
-				p_DArr = new T[size];
-				for (int i = 0; i < size; i++)
-				{
-					p_DArr[i] = 0;
-				}
-				break;
-			}
-			catch (const std::bad_alloc&)
-			{
-				cout << "Error: bad_alloc" << endl;
-			}
+			p_DArr[i] = 0;
 		}
 		initialized = true;
 	}
@@ -99,42 +106,24 @@ public:
 
 	void SetArr(int index, T val)
 	{
-		try
+		if (val < -100 || val > 100)
 		{
-			if (val < -100 || val > 100)
-			{
-				throw std::invalid_argument("Error: invalid argument");
-			}
-			if (index < 0 || index >= size)
-			{
-				throw std::out_of_range("Error: out of range");
-			}
-			p_DArr[index] = val;
+			throw std::invalid_argument("Error: invalid argument");
 		}
-		catch (std::out_of_range& error)
+		if (index < 0 || index >= size)
 		{
-			cout << error.what() << endl;
+			throw std::out_of_range("Error: out of range");
 		}
-		catch (std::invalid_argument& error)
-		{
-			cout << error.what() << endl;
-		}
+		p_DArr[index] = val;
 	}
 
 	int GetArr(int index)
 	{
-		try
+		if (index < 0 || index >= size)
 		{
-			if (index < 0 || index >= size)
-			{
-				throw std::out_of_range("Error: out of range");
-			}
-			return p_DArr[index];
+			throw std::out_of_range("Error: out of range");
 		}
-		catch (std::out_of_range& error)
-		{
-			cout << error.what() << endl;
-		}
+		return p_DArr[index];
 	}
 
 	void show()
@@ -184,55 +173,30 @@ public:
 	}
 };
 
-DArr operator + (const DArr& a, const DArr& b)
-{
-	DArr temp;
-
-	if (a.size < b.size)
-	{
-		temp.init(a.size);
-		for (int i = 0; i < a.size; i++)
-		{
-			temp.p_DArr[i] = a.p_DArr[i] + b.p_DArr[i];
-		}
-	}
-
-	else
-	{
-		temp.init(b.size);
-		for (int i = 0; i < a.size; i++)
-		{
-			temp.p_DArr[i] = a.p_DArr[i] + b.p_DArr[i];
-		}
-	}
-
-	return temp;
-}
-
-void DArr::operator = (const DArr& a)
-{
-	if (a.initialized)
-	{
-		this->size = a.size;
-		p_DArr = new T[size];
-		for (int i = 0; i < size; i++)
-		{
-			this->p_DArr[i] = a.p_DArr[i];
-		}
-		this->initialized = true;
-	}
-	else
-	{
-		cout << "Error: Copied array does not initialize" << endl;
-	}
-}
-
 int main()
 {
-	DArr<int> array(5);
-	DArr<int> array2(array);
-	DArr<int> array3(5);
-	array3 = array + array2;
-	array3.show();
-	return 0;
+	try
+	{
+		DArr<int> array(5);
+		array.GetArr(6);
+		array.SetArr(4, 101);
+		DArr<int> array2(array);
+		DArr<int> array3(5);
+		array3 = array + array2;
+		array3.show();
+		return 0;
+	}
+
+	catch (const std::bad_alloc&)
+	{
+		cout << "Error: bad_alloc" << endl;
+	}
+	catch (std::out_of_range& error)
+	{
+		cout << error.what() << endl;
+	}
+	catch (std::invalid_argument& error)
+	{
+		cout << error.what() << endl;
+	}
 }
